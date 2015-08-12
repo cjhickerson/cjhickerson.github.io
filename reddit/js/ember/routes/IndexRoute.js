e8aefc3ad60935e805d64f86475e5cc5cb671085
Chris Hickerson <chickerson@degdigital.com>
2015-08-12 13:35:39 -0500

    remove reddit demo folder

--- reddit/js/ember/routes/IndexRoute.js
+++
@@ -1,9 +0,0 @@
-App.IndexRoute = Ember.Route.extend({
-    redirect: function() {
-        if ($.cookie('subreddit')) {
-            this.transitionTo('/paginated/' + $.cookie('subreddit') + "/0/25");
-        } else {
-            this.transitionTo('/paginated/frontpage/0/25');
-        }
-    }
-});
